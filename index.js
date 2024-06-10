65d343c407826388ae473287dd406cb6909ef43d
TheEmeraldEnd <78377336+TheEmeraldEnd@users.noreply.github.com>
2024-06-10 00:21:47 -0500

    Attempt number 4

--- index.js
+++ index.js
@@ -11,16 +11,16 @@
 }
 
 //Sends contents to the webhook
-function SendContentToDiscord(contents) {
-    const webhook =
-    'https://discord.com/api/webhooks/1249497936153280653/8VsBFqRxWQ97si__bR6O4ynlmwQSgecfYWiKSAvY_sFHQqODhXI0K-6ZoUmUeg8G7gr8';
+//function SendContentToDiscord(contents) {
+//    const webhook =
+//    'https://discord.com/api/webhooks/1249497936153280653/8VsBFqRxWQ97si__bR6O4ynlmwQSgecfYWiKSAvY_sFHQqODhXI0K-6ZoUmUeg8G7gr8';
 
-    const request = new XMLHttpRequest();
-    request.open('POST', webhook);
-    request.setRequestHeader('Content-type', 'application/json');
-    const params = {
-        content: contents
-    };
+//    const request = new XMLHttpRequest();
+//    request.open('POST', webhook);
+//    request.setRequestHeader('Content-type', 'application/json');
+//    const params = {
+//        content: contents
+//    };
 
-    request.send(JSON.stringify(params));
-}
+//    request.send(JSON.stringify(params));
+//}
