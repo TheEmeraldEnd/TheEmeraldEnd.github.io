7613aeff0136064a022d6f47d2d29876d38328fb
TheEmeraldEnd <78377336+TheEmeraldEnd@users.noreply.github.com>
2025-02-02 21:36:27 -0600

    Recipe listing works and takes to recipe page

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 let recipeElementButtonBase = document.getElementById('recipeElementBase');
 let recipeElementList = document.getElementById('recipeElementList');
 
-for(let i = 0; i < 4; i++){
-    let newRecipeElement = recipeElementButtonBase.cloneNode(true);
+function LoadWindow(){
+    window.location='Recipes/recipe.html';
+}
 
+//list of recipes inside of the recipe's list (must be added in manually)
+let recipeFolders = [
+    'Carbonara (Eggless)',
+    'Eggs In a Basket Sandwiches'
+];
+
+//format the list (doesn't set the list when clicked yet)
+for(let i = 0; i < recipeFolders.length; i++){
+    let newRecipeElement = recipeElementButtonBase.cloneNode(true);
+    newRecipeElement.children[0].textContent = `${recipeFolders[i]}`;
+    newRecipeElement.children[0].onclick = function() {LoadWindow();};
     recipeElementList.appendChild(newRecipeElement);
 }
+recipeElementButtonBase.remove();
