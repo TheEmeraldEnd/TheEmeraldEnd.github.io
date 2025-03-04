43d1cec4bb5130cd7952fcea252d5d6405c79fbc
TheEmeraldEnd <78377336+TheEmeraldEnd@users.noreply.github.com>
2025-03-04 12:07:18 -0600

    Added description (not working) and cleaned up index.js a bit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let recipeElementButtonBase = document.getElementById('recipeElementBase');
 let recipeElementList = document.getElementById('recipeElementList');
 
+//Tags
+let recipeButtonTag = "recipeElement"
+let recipeImageTag = "finishedProductImage"
+
 //Defign functions
 function LoadWindow(incomingRecipeTitleString){
     window.location=`Recipes/${incomingRecipeTitleString}/recipe.html`;
@@ -9,6 +13,17 @@ function LoadWindow(incomingRecipeTitleString){
 function SetFinishedResult(incomingRecipeTitleString){
     let finishedProduct = `Recipes/${incomingRecipeTitleString}/Finished.jpg`
     return finishedProduct;
+}
+
+//childClassTag should be a string
+//parentElement should be an element
+//returns the child element
+function SearchThroughDescendants(childClassTag, parentElement){
+    for(let i = 0; i < parentElement.children.length; i++){
+        let child = parentElement.children[i]
+        if (child.classname === childClassTag)
+            return child; 
+    }
 }
 
 //list of recipes inside of the recipe's list (must be added in manually)
@@ -24,25 +39,25 @@ recipeFolders.sort();
 //format the list (doesn't set the list when clicked yet)
 for(let i = 0; i < recipeFolders.length; i++){
     let newRecipeElement = recipeElementButtonBase.cloneNode(true);
-    newRecipeElement.children[0].textContent = `${recipeFolders[i]}`;
-    newRecipeElement.children[0].onclick = function() {LoadWindow(recipeFolders[i]);};
-    newRecipeElement.children[1].src = SetFinishedResult(recipeFolders[i]);
+    SearchThroughDescendants(recipeButtonTag).textContent = `${recipeFolders[i]}`;
+    SearchThroughDescendants(recipeButtonTag).onclick = function() {LoadWindow(recipeFolders[i]);};
+    SearchThroughDescendants(recipeImageTag).src = SetFinishedResult(recipeFolders[i]);
     recipeElementList.appendChild(newRecipeElement);
 }
 recipeElementButtonBase.remove();
 
 //Set the recipes while typing
-let recipeButtons = document.getElementsByClassName('recipeElement')
+let recipeElements = document.getElementsByClassName('recipeElement')
 
 function LoadEnteredInput(incomingText){
-    for(let i = 0; i < recipeButtons.length; i++){
-        var recipeTitle = recipeButtons[i].textContent;
+    for(let i = 0; i < recipeElements.length; i++){
+        var recipeTitle = recipeElements[i].textContent;
 
         if (recipeTitle.toUpperCase().includes(incomingText.toUpperCase())){
-            recipeButtons[i].parentElement.style.display = 'inline';
+            recipeElements[i].parentElement.style.display = 'inline';
         }
         else{
-            recipeButtons[i].parentElement.style.display = 'none';
+            recipeElements[i].parentElement.style.display = 'none';
         }
     }
 }
