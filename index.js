fcf2b5cac037d29a439af628eed4395ebad87b55
TheEmeraldEnd <endersage1135@gmail.com>
2026-03-31 02:55:32 -0500

    Updated index.js to allow generation
    Added:
    - index.js can generate html elements to display projects
    
    TODO:
    - index.js to call upon actual projects instead of the test set.

--- index.js
+++ index.js
@@ -1,3 +1,93 @@
-console.log("Content");
 
 window.location.href = "Static Website Projects/Recipe Project/index.html";
+
+let projectListListID = "ProjectListListID";
+
+//Project Display
+let projectListListElement = document.getElementById(projectListListID);
+
+class ProjectCreationFunctions{
+    static MakeProjectCatagoryDisplay(incomingProjectCatagoryString, incomingFormattedProjectElements, incomingCatagorySearchLink){
+        //Ignore incomingCatagorySearchLink for now
+        if (incomingCatagorySearchLink == ""){
+
+        }
+        else{
+
+        }
+        
+        let tempElement = "";
+        tempElement += 
+            `<p class="ProjectCatagoryNameDisplay">` +
+                `${incomingProjectCatagoryString}` +
+            `</p>`;
+
+        tempElement += `<div class="ProjectListFlexRowContainer ProjectCatagoryListDisplay">`
+        
+        for(let i = 0; i < incomingFormattedProjectElements.length; i++){
+            tempElement += incomingFormattedProjectElements[i];
+        }
+
+        tempElement += 
+            `<button>` + 
+            `<img src="IndexImages/SearchForImage.png" title="To ${incomingProjectCatagoryString} Search">` +
+            `</button>`
+
+        tempElement += `</div>`;
+        
+        return tempElement;
+            
+    }
+
+    static MakeImageElement(incomingImageLink){
+        if (incomingImageLink === ''){
+            incomingImageLink = "IndexImages/Default/DefaultProjectImage.png";
+        }
+
+        let tempElement = 
+            '<div class="ProjectFlexColumnItemElement ProjectImageWrapper ProjectListFlexRowContainer">' +
+                    '<div class="ProjectListFlexRowItem ProjectImageWrapperLeft"></div>' +
+                    `<img src="${incomingImageLink}" class="ProjectImage ProjectListFlexRowItem">` +
+                    '<div class="ProjectListFlexRowItem ProjectImageWrapperRight"></div>' +
+                '</div>';
+
+        return tempElement;
+    }
+
+    static MakeTitleElement(incomingProjectTitle){
+        let tempElement = 
+            '<p class="ProjectFlexColumnItemElement ProjectTitleElement">' +
+            `${incomingProjectTitle}` +
+            '</p>';
+
+        return tempElement;
+    }
+
+    static MakeSummaryElement(incomingProjectSummary){
+        let tempElement = 
+            '<p class="ProjectFlexColumnItemElement ProjectSummaryElement">' +
+            `${incomingProjectSummary}` +
+            '</p>';
+
+        return tempElement;
+    }
+
+    static MakeProjectDisplay(incomingProjectTitle, incomingProjectSummary, incomingProjectImageLink, incomingProjectLink){
+        let tempElement = `<a class="ProjectFlexColumnContainer ProjectListFlexRowItem ProjectDisplayWrapper" href="${incomingProjectLink}"` +
+                `title="Go To ${incomingProjectTitle}">`;
+        tempElement += this.MakeImageElement(incomingProjectImageLink);
+        tempElement += this.MakeTitleElement(incomingProjectTitle);
+        tempElement += this.MakeSummaryElement(incomingProjectSummary);
+        tempElement += '</a>';
+
+
+        return tempElement;
+    }
+
+}
+
+let tempProjectList = [
+    ProjectCreationFunctions.MakeProjectDisplay('Test title', 'Some summary', '', 'https://github.com/TheEmeraldEnd/LegoTestSite')
+];
+
+projectListListElement.innerHTML += ProjectCreationFunctions.MakeProjectCatagoryDisplay("Test Catagory", tempProjectList, "Temp Link");
